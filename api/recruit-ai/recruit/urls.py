e760b2fa382dceabb2df29259d491c5c554c7a8b
Omkar Yadav <omyad21@icstudents.org>
2021-04-18 11:17:26 -0500

    wrote api call for player comparison

--- api/recruit-ai/recruit/urls.py
+++ api/recruit-ai/recruit/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('year/<int:year>', views.query_year, name='query_year'),
     path('name/<str:name>', views.query_name, name='query_name'),
     path('college/<str:college>', views.query_college, name='query_college'),
+    path('comparison/<str:name>', views.get_player_comparison, name='get_player_comparison'),
     path('all', views.all, name = 'all'),
     path('', views.index, name='index'),
 ]
